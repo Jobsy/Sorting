70a1f1ee3113e7dddaec92d1c5002b87ff3bcecd
Oluwajoba <oluwajobabello@gmail.com>
2020-02-19 13:30:29 +0100

    complete binary_search / seaching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -21,6 +21,17 @@ def binary_search(arr, target):
     high = len(arr)-1
 
     # TO-DO: add missing code
+    while low <= high:
+        mid = low + (high - low)/2
+        # Check if target is present at mid
+        if arr[mid] == target:
+            return mid
+        # If target is greater, ignore left half
+        elif arr[mid] < target:
+            low = mid + 1
+        # If target is smaller, ignore right half
+        else:
+            high = mid - 1
 
     return -1  # not found
 
